4af2537bdf5ec2bef4b1c40f00e094df1ad41757
Codex.IO <189389677+CodexClub567@users.noreply.github.com>
2025-04-22 05:20:04 +0600

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>OML Tools Plugin</h1>
+        <p>
+          Welcome to the OML Tools Plugin frontend.  This is a placeholder.
+        </p>
+        {/* You would add your React components here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
